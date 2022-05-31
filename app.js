5ee009d5498b61c1a92c8d01a9fa9f00e0c289a4
princania <premmahato7488@gmail.com>
2022-05-31 20:57:01 +0530

    resolvve 3

--- app.js
+++ app.js
@@ -70,8 +70,6 @@ app.post("/compose", function(req,res){
   res.redirect("/");
 })
 
-let port = process.env.PORT;
-if(port==null || port == ""){
-  port = 3000;
-}
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server has started");
+});
